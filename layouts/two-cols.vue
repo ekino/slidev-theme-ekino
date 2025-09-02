f7ca392c728e7ac96281ddf99c9fc55a53e45afd
maxime.dubourg <maxime.dubourg@ekino.com>
2025-09-02 11:49:58 +0200

    feat:  layout

---
+++ layouts/two-cols.vue
@@ -0,0 +1,19 @@
+<script setup>
+import MainLayout from "../components/main-layout.vue";
+</script>
+
+<template>
+  <MainLayout name="two-cols">
+    <slot />
+
+    <div v-if="$slots.left || $slots.right" class="grid grid-cols-2 gap-x-6">
+      <div v-if="$slots.left || $slots.right">
+        <slot name="left" />
+      </div>
+
+      <div v-if="$slots.right">
+        <slot name="right" />
+      </div>
+    </div>
+  </MainLayout>
+</template>
