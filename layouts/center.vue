af02980bbe09900cb24e438d649f9b6b7a2793d9
theo.gillet <theo.gillet@ekino.com>
2026-02-11 10:40:11 +0100

    feat: added custom layout center

---
+++ layouts/center.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "../components/main-layout.vue";
+</script>
+
+<template>
+  <MainLayout name="center">
+    <div class="flex flex-col items-center justify-center h-full">
+      <slot />
+    </div>
+  </MainLayout>
+</template>
